e224241b9f7337f034676a37a85ef3b47320d24b
Bazaah <github@luxolus.com>
2020-11-08 08:07:39 +0000

    improve IgnoredAny usage

--- src/visitor.rs
+++ src/visitor.rs
@@ -1,6 +1,6 @@
 use {
     super::RawJson,
-    serde::de::{Error, IgnoredAny, MapAccess, SeqAccess, Visitor},
+    serde::de::{Deserialize, Error, IgnoredAny, MapAccess, SeqAccess, Visitor},
     std::{fmt, str::FromStr},
 };
 
@@ -33,54 +33,21 @@ impl<'de, 'p> Visitor<'de> for LazyVisitor<'p> {
         )
     }
 
-    fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+    fn visit_map<A>(self, map: A) -> Result<Self::Value, A::Error>
     where
         A: MapAccess<'de>,
     {
-        let mut out = None;
-
-        while let Some((k, v)) = map.next_entry::<&str, &RawJson>()? {
-            if k == self.k_or_idx {
-                out = Some(v);
-                break;
-            }
-        }
-
-        while let Some((_k, _v)) = map.next_entry::<IgnoredAny, IgnoredAny>()? {}
-
-        out.ok_or_else(|| {
-            Error::custom(format!(
-                "unable to locate a value with the key: {}",
-                self.k_or_idx
-            ))
-        })
+        internal_visit_map(self.k_or_idx, map)
     }
 
-    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+    fn visit_seq<A>(self, seq: A) -> Result<Self::Value, A::Error>
     where
         A: SeqAccess<'de>,
     {
         let index = u64::from_str(self.k_or_idx)
             .map_err(|_| Error::custom(format!("not an indexable number: {}", self.k_or_idx)))?;
-        let mut count = 0;
-        let mut out = None;
 
-        while let Some(val) = seq.next_element()? {
-            if count == index {
-                out = Some(val);
-                break;
-            }
-            count += 1;
-        }
-
-        while let Some(_val) = seq.next_element::<IgnoredAny>()? {}
-
-        out.ok_or_else(|| {
-            Error::custom(format!(
-                "unable to locate a value with the index: {}",
-                index
-            ))
-        })
+        internal_visit_seq(index, seq)
     }
 }
 
@@ -102,27 +69,11 @@ impl<'de, 'p> Visitor<'de> for MapVisitor<'p> {
         write!(formatter, "a map including the key: {}", self.key)
     }
 
-    fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+    fn visit_map<A>(self, map: A) -> Result<Self::Value, A::Error>
     where
         A: MapAccess<'de>,
     {
-        let mut out = None;
-
-        while let Some((k, v)) = map.next_entry::<&str, &RawJson>()? {
-            if k == self.key {
-                out = Some(v);
-                break;
-            }
-        }
-
-        while let Some((_k, _v)) = map.next_entry::<IgnoredAny, IgnoredAny>()? {}
-
-        out.ok_or_else(|| {
-            Error::custom(format!(
-                "unable to locate a value with the key: {}",
-                self.key
-            ))
-        })
+        internal_visit_map(self.key, map)
     }
 }
 
@@ -144,29 +95,73 @@ impl<'de> Visitor<'de> for ArrayVisitor {
         write!(formatter, "an array including the index: {}", self.index)
     }
 
-    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+    fn visit_seq<A>(self, seq: A) -> Result<Self::Value, A::Error>
     where
         A: SeqAccess<'de>,
     {
-        let mut count = 0;
-        let mut out = None;
-
-        while let Some(val) = seq.next_element()? {
-            if count == self.index {
-                out = Some(val);
-                break;
-            }
-            count += 1;
-        }
-
-        while let Some(_val) = seq.next_element::<IgnoredAny>()? {}
-
-        out.ok_or_else(|| {
-            Error::custom(format!(
-                "unable to locate a value with the index: {}",
-                self.index
-            ))
-        })
+        internal_visit_seq(self.index, seq)
     }
 }
 
+fn internal_visit_map<'de, A, T>(target: &str, map: A) -> Result<T, A::Error>
+where
+    T: Deserialize<'de>,
+    A: MapAccess<'de>,
+{
+    let mut map = map;
+    let mut out = None;
+
+    // Serde expects each call to next_key be followed by a
+    // call to next_value before another call to next_key, however
+    // we only want to spend effort deserializing if this is the
+    // expected key, otherwise we forward to IgnoredAny.
+    while let Some(k) = map.next_key::<&str>()? {
+        match k == target {
+            false => {
+                map.next_value::<IgnoredAny>()?;
+            }
+            true => {
+                out = Some(map.next_value()?);
+                break;
+            }
+        }
+    }
+
+    // It is a logic error to not fully walk a given map, however we have
+    // already found our value here so we ignore everything except potential
+    // errors in the stream.
+    while let Some((_k, _v)) = map.next_entry::<IgnoredAny, IgnoredAny>()? {}
+
+    out.ok_or_else(|| Error::custom(format!("unable to locate a value with the key: {}", target)))
+}
+
+fn internal_visit_seq<'de, A, T>(target: u64, seq: A) -> Result<T, A::Error>
+where
+    T: Deserialize<'de>,
+    A: SeqAccess<'de>,
+{
+    let mut seq = seq;
+    let mut count = 0;
+
+    // Ignore all elements before our target
+    while count < target {
+        if seq.next_element::<IgnoredAny>()?.is_none() {
+            break;
+        }
+        count += 1;
+    }
+
+    let out = seq.next_element()?;
+
+    // It is a logic error to not fully walk a given sequence, however we
+    // have already found our value, so we ignore everything except potential
+    // errors.
+    while let Some(_val) = seq.next_element::<IgnoredAny>()? {}
+
+    out.ok_or_else(|| {
+        Error::custom(format!(
+            "unable to locate a value with the index: {}",
+            target
+        ))
+    })
+}
